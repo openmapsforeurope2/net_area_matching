2415218b6497fe0698347938f725de1dd2d15ff5
Samir Khazaz <samir.khazaz@ign.fr>
2024-03-21 09:18:20 +0100

    ajout step attribut

---
+++ include/app/calcul/SetAttributeMergedAreasOp.h
@@ -0,0 +1,71 @@
+#ifndef _APP_CALCUL_SETATTRIBUTEMERGEDAREASOP_H_
+#define _APP_CALCUL_SETATTRIBUTEMERGEDAREASOP_H_
+
+//SOCLE
+#include <ign/feature/sql/FeatureStorePostgis.h>
+#include <ign/tools/stringtools.h>
+
+//EPG
+#include <epg/log/EpgLogger.h>
+#include <epg/log/ShapeLogger.h>
+
+
+namespace app{
+namespace calcul{
+
+	class SetAttributeMergedAreasOp {
+
+	public:
+
+	
+        /// @brief 
+        /// @param borderCode 
+        /// @param verbose 
+		SetAttributeMergedAreasOp(
+			std::string const& borderCode,
+            bool verbose
+        );
+
+        /// @brief 
+        ~SetAttributeMergedAreasOp();
+
+
+		/// \brief
+		static void compute(
+			std::string const& borderCode,
+			bool verbose
+		);
+
+
+	private:
+		//--
+		std::string                                              _borderCode;
+		//--
+		std::vector<std::string>                                 _vCountry;
+		//--
+		ign::feature::sql::FeatureStorePostgis*                  _fsArea;
+		//--
+		epg::log::EpgLogger*                                     _logger;
+		//--
+		epg::log::ShapeLogger*                                   _shapeLogger;
+		//--
+		bool                                                     _verbose;
+
+
+	private:
+
+		//--
+		void _init(std::string const& borderCode);
+
+		//--
+		void _compute() const;
+
+
+
+
+    };
+
+}
+}
+
+#endif
---
+++ src/app/calcul/SetAttributeMergedAreasOp.cpp
@@ -0,0 +1,116 @@
+// APP
+#include <app/calcul/SetAttributeMergedAreasOp.h>
+#include <app/params/ThemeParameters.h>
+#include <app/tools/zTools.h>
+
+// BOOST
+#include <boost/progress.hpp>
+
+// EPG
+#include <epg/Context.h>
+#include <epg/params/EpgParameters.h>
+#include <epg/sql/tools/numFeatures.h>
+#include <epg/sql/DataBaseManager.h>
+#include <epg/tools/StringTools.h>
+#include <epg/tools/TimeTools.h>
+#include <epg/tools/FilterTools.h>
+
+
+namespace app
+{
+    namespace calcul
+    {
+        ///
+        ///
+        ///
+        SetAttributeMergedAreasOp::SetAttributeMergedAreasOp(
+            std::string const& borderCode,
+            bool verbose
+        ) : 
+            _verbose(verbose),
+            _borderCode(borderCode)
+        {
+            _init(borderCode);
+        }
+
+        ///
+        ///
+        ///
+        SetAttributeMergedAreasOp::~SetAttributeMergedAreasOp()
+        {
+        }
+
+        ///
+        ///
+        ///
+        void SetAttributeMergedAreasOp::compute(
+            std::string const& borderCode,
+			bool verbose
+		) {
+            SetAttributeMergedAreasOp intersectingAreasMergerOp(borderCode, verbose);
+            intersectingAreasMergerOp._compute();
+        }
+
+        ///
+        ///
+        ///
+        void SetAttributeMergedAreasOp::_init(std::string const& borderCode)
+        {
+            //--
+            _logger = epg::log::EpgLoggerS::getInstance();
+            _logger->log(epg::log::INFO, "[START] initialization: " + epg::tools::TimeTools::getTime());
+
+            //--
+            epg::Context *context = epg::ContextS::getInstance();
+
+            // epg parameters
+            epg::params::EpgParameters const& epgParams = context->getEpgParameters();
+            std::string const areaTableName = epgParams.getValue(AREA_TABLE).toString();
+            std::string const idName = epgParams.getValue(ID).toString();
+            std::string const geomName = epgParams.getValue(GEOM).toString();
+
+            //--
+            _fsArea = context->getDataBaseManager().getFeatureStore(areaTableName, idName, geomName);
+
+            //--
+            _logger->log(epg::log::INFO, "[END] initialization: " + epg::tools::TimeTools::getTime());
+
+            //--
+		    epg::tools::StringTools::Split(borderCode, "#", _vCountry);
+        }
+
+
+        ///
+        ///
+        ///
+        void SetAttributeMergedAreasOp::_compute() const {
+
+            // epg parameters
+            epg::params::EpgParameters const& epgParams = epg::ContextS::getInstance()->getEpgParameters();
+            std::string const countryCodeName = epgParams.getValue(COUNTRY_CODE).toString();
+            std::string const idName = epgParams.getValue(ID).toString();
+
+            ign::feature::FeatureFilter filterArea(countryCodeName+"='%#%'");
+
+            int numFeatures = epg::sql::tools::numFeatures(*_fsArea, filterArea);
+            boost::progress_display display(numFeatures, std::cout, "[ set attribute merged areas % complete ]\n");
+
+            ign::feature::FeatureIteratorPtr itArea = _fsArea->getFeatures(filterArea);
+
+            std::list<std::set<std::string>> lsAreas2Merge;
+            std::set<std::string> sTreatedArea;
+            while (itArea->hasNext())
+            {
+                ++display;
+
+                ign::feature::Feature const& fArea = itArea->next();
+                ign::geometry::MultiPolygon const& mp = fArea.getGeometry().asMultiPolygon();
+                std::string idOrigin = fArea.getId();
+
+            }
+
+        }
+
+
+    }
+}
