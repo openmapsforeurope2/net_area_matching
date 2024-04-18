d65fca516d36d52a94d112e2ea577406d6ddc9e8
Samir Khazaz <samir.khazaz@ign.fr>
2024-04-18 15:25:34 +0200

    resolution conflits theme param

--- src/app/params/ThemeParameters.cpp
+++ src/app/params/ThemeParameters.cpp
@@ -31,8 +31,9 @@ namespace params{
 		_initParameter( DIST_SNAP_MERGE_CF, "DIST_SNAP_MERGE_CF" );
 		_initParameter( PS_BORDER_OFFSET, "PS_BORDER_OFFSET" );
 		_initParameter(PC_DISTANCE_THRESHOLD, "PC_DISTANCE_THRESHOLD"); 
-		_initParameter(THRESHOLD_AREA_ATTR, "THRESHOLD_AREA_ATTR");
-
+		_initParameter(SAM_SMALL_AREA_THRESHOLD, "SAM_SMALL_AREA_THRESHOLD");
+			
+		_initParameter(THRESHOLD_AREA_ATTR, "THRESHOLD_AREA_ATTR"); 
 		_initParameter(LIST_ATTR_TO_CONCAT, "LIST_ATTR_TO_CONCAT");
 		_initParameter(LIST_ATTR_JSON, "LIST_ATTR_JSON");
 		_initParameter(LIST_ATTR_W, "LIST_ATTR_W");
